6340c4c279654b34ffb96bc6ef928db41bd12610
Ernest DeFoy <erniedefoy3@gmail.com>
2020-10-29 23:06:08 -0400

    Initial Commit

---
+++ QuiteAdaptableKit/QuiteAdaptableKit.h
@@ -0,0 +1,18 @@
+//
+//  QuiteAdaptableKit.h
+//  QuiteAdaptableKit
+//
+//  Created by Ernest DeFoy on 10/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuiteAdaptableKit.
+FOUNDATION_EXPORT double QuiteAdaptableKitVersionNumber;
+
+//! Project version string for QuiteAdaptableKit.
+FOUNDATION_EXPORT const unsigned char QuiteAdaptableKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuiteAdaptableKit/PublicHeader.h>
+
+
